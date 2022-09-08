10649a40c9ea746094dc96fca972ba9122a70228
kkiniuss <kkiniuss03@gmail.com>
2022-09-08 21:58:50 +0300

    fix

--- adverts_action.py
+++ adverts_action.py
@@ -58,7 +58,7 @@ def get_amounts(paymethod, min_amount, key, email, proxy, asset='BTC', fiat='RUB
 
 @catch_error
 def parse_average_amount(amounts_info):
-    print(amounts_info)
+    # print(amounts_info)
     sum_amounts = 0
     for amount in amounts_info:
         sum_amounts += float(amount['rate'])
