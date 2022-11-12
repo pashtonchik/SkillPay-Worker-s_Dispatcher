959e5d71bfa1c16dc42e0b51a68ee9d2a5163c20
pashtonchik <pashka191@yandex.ru>
2022-11-12 21:29:49 +0300

    asdasd

--- connector.py
+++ connector.py
@@ -8,7 +8,7 @@ import requests
 from log import logger
 from server import URL_DJANGO
 
-URL_FLASK = 'http://127.0.0.1:5000/'
+URL_FLASK = 'http://127.0.0.1:5001/'
 url_error = URL_DJANGO + 'api/error/'
 
 
